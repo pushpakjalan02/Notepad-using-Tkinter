4c79e712f05614b3205f4b1db9947ca87a6257e2
Pushpak Jalan <pushpakjalan02@gmail.com>
2019-07-09 11:02:45 +0530

    Second Commit to main_file.py
    Added Save, Save As and Open Options to the Notepad

--- main_file.py
+++ main_file.py
@@ -1,4 +1,6 @@
 import tkinter
+import os
+from tkinter.filedialog import *
 
 window = tkinter.Tk()
 window.title('Notepad by Pushpak')
@@ -7,7 +9,47 @@ menu = tkinter.Menu(window)
 
 text_area = tkinter.Text(window, undo = True)    
 
+file_name = None
+
+def open_function():
+    global window, menu, text_area, file_name
+    file_name = askopenfilename(defaultextension = ".txt", filetypes = [("All Files","*.*"),("Text Documents","*.txt")])
+    if(file_name == ""):
+        file_name = None
+    else:
+        window.title(os.path.basename(file_name) + " - Notepad")
+        text_area.delete(1.0, END)
+        open_file = open(file_name, "r")
+        text_area.insert(1.0,open_file.read())
+        open_file.close()
+    return
+
+def save_function():
+    global window, menu, text_area, file_name
+    if(file_name == None):
+        file_name = asksaveasfilename(initialfile="Untitled.txt", defaultextension = ".txt", filetypes = [("All Files","*.*"),("Text Documents","*.txt")])
+        if(file_name == ""):
+            file_name = None
+    if(file_name != None):
+        save_file = open(file_name, "w")
+        save_file.write(text_area.get(1.0, END))
+        save_file.close()
+        window.title(os.path.basename(file_name) + " - Notepad")
+        return
+
+def save_as_function():
+    global window, menu, text_area, file_name
+    temp_file_name = asksaveasfilename(initialfile = "Untitled.txt", defaultextension = ".txt", filetypes = [("All Files","*.*"),("Text Documents",".txt")])
+    if(temp_file_name != ""):
+        file_name = temp_file_name
+        save_file = open(file_name, "w")
+        save_file.write(text_area.get(1.0, END))
+        save_file.close()
+        window.title(os.path.basename(file_name) + " - Notepad")
+    return
+
 def copy():
+    global window, menu, text_area, file_name
     text_area.clipboard_clear()
     try:
         copy_val = text_area.selection_get()
@@ -19,6 +61,7 @@ def copy():
         return
 
 def cut():
+    global window, menu, text_area, file_name
     try:
         copy()
         text_area.delete(tkinter.SEL_FIRST, tkinter.SEL_LAST)
@@ -28,6 +71,7 @@ def cut():
         return
 
 def paste():
+    global window, menu, text_area, file_name
     paste_val = text_area.clipboard_get()
     try:
         if len(tkinter.SEL) > 0:
@@ -39,9 +83,9 @@ def paste():
 
 def file_menu(file):
     file.add_command(label='New')
-    file.add_command(label='Open')
-    file.add_command(label='Save')
-    file.add_command(label='Save As')
+    file.add_command(label='Open', command = open_function)
+    file.add_command(label='Save', command = save_function)
+    file.add_command(label='Save As', command = save_as_function)
     file.add_command(label='Close')
     return
 
@@ -83,6 +127,8 @@ def main_menu(menu):
     return
 
 def main():
+    global window, menu, text_area, file_name
+    
     main_menu(menu)
     
     text_area.pack(expand = True, fill = 'both')
