ee8e065506947b72873db9ccee6dc28fd097be3c
Pushpak Jalan <pushpakjalan02@gmail.com>
2019-07-23 12:31:38 +0530

    Add files via upload

---
+++ Practice Files/Pushpak.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+import os
+from tkinter import *
+
+# To get the sppace for above message
+from tkinter.messagebox import *
+# message box is used to write the message in the white
+# called notepad.
+
+# To get the dialog box opened when required
+from tkinter.filedialog import *
+# filedialog is used for dialog box to appear while opening and
+# saving file from or to a particular place in the system
